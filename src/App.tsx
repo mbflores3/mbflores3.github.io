eab38c683e80397a312548ef1629b596c52311ba
Mario Flores <62151201+mbflores3@users.noreply.github.com>
2026-09-07 16:01:35 -0300

    Uncomment project routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import EnaexRobotics from "./pages/projects/EnaexRobotics";
+import SpaeGroup from "./pages/projects/SpaeGroup";
+import TarLab from "./pages/projects/TarLab";
+import 2xAutomation from "./pages/projects/2xAutomation";
 import RobotX from "./pages/projects/RobotX";
+import Mariscope from "./pages/projects/Mariscope";
+import IpreUC from "./pages/projects/IpreUC";
+import IpreUSACH from "./pages/projects/IpreUSACH";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,13 +25,13 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects/enaexrobotics" element={<EnaexRobotics />} />
-          {/*<Route path="/projects/spaegroup" element={<SpaeGroup />} /> */}
-          {/*<Route path="/projects/tarlab" element={<TarLab />} /> */}
-          {/*<Route path="/projects/2xautomation" element={<2xAutomation />} /> */}
-          <Route path="/projects/robotx" element={<RobotX />} />
-          {/*<Route path="/projects/mariscope" element={<Mariscope />} /> */}
-          {/*<Route path="/projects/uc" element={<UC />} /> */}
-          {/*<Route path="/projects/usach" element={<USACH />} /> */}
+          <Route path="/projects/spaegroup" element={<SpaeGroup />} />
+          <Route path="/projects/tarlab" element={<TarLab />} />
+          <Route path="/projects/2xautomation" element={<2xAutomation />} />
+          <Route path="/projects/robotx" element={<RobotX />}
+          <Route path="/projects/mariscope" element={<Mariscope />} />
+          <Route path="/projects/uc" element={<UC />} />
+          <Route path="/projects/usach" element={<USACH />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
