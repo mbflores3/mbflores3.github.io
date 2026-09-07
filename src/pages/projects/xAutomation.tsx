199e68f4ed21db828fbed82193e02f5bc1393a7b
Mario Flores <62151201+mbflores3@users.noreply.github.com>
2026-09-07 16:05:36 -0300

    Rename component from 2xAutomation to xAutomation

--- src/pages/projects/xAutomation.tsx
+++ src/pages/projects/xAutomation.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const 2xAutomation = () => {
+const xAutomation = () => {
   return (
     <main className="min-h-screen bg-background text-foreground">
       <div className="max-w-5xl mx-auto px-6 py-16">
@@ -104,4 +104,4 @@ const 2xAutomation = () => {
   );
 };
 
-export default 2xAutomation;
+export default xAutomation;
