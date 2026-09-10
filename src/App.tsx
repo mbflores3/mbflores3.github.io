9fa5e956d2ee3a6b54d341e866ad587778265b08
Mario Flores <62151201+mbflores3@users.noreply.github.com>
2026-09-10 11:09:28 -0300

    Remove IpreUSACH route from App.tsx
    Removed IpreUSACH route from the project routes.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import TarLab from "./pages/projects/TarLab";
 import Xautomation from "./pages/projects/Xautomation";
 import RobotX from "./pages/projects/RobotX";
 import Mariscope from "./pages/projects/Mariscope";
-import IpreUSACH from "./pages/projects/IpreUSACH";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -29,7 +28,6 @@ const App = () => (
           <Route path="/projects/xautomation" element={<Xautomation />} />
           <Route path="/projects/robotx" element={<RobotX />} />
           <Route path="/projects/mariscope" element={<Mariscope />} />
-          <Route path="/projects/ipreusach" element={<IpreUSACH />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
